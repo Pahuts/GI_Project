4da09c19ee0ae8c5f294b961b637e2f7b532d7f7
Pahuts <luispahutan@gmail.com>
2019-06-10 22:24:18 +0800

    2nd commit

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
     <head>
       <title> GrandInfinity Variety Trading</title>
       <link href="css/styles.css" rel="stylesheet" type="text/css">
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   
        <!-- Latest compiled and minified CSS -->
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
@@ -13,26 +14,72 @@
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 
       <!-- Latest compiled JavaScript -->
+      <script src ="js/caroufredsel.js" type ="text/javascript"></script>
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     </head> 
   <body>
 
     <header>
-      <div class="container">
-        <div class="row">
-          <a href="" class="logo"><img src="images/logo.png"></a> 
-          <nav>
-            <ul>
-              <li><a href ="">Home</a></li>
-              <li><a href ="">Work</a></li>
-              <li><a href ="">Services</a></li>
-              <li><a href ="">Clients</a></li>
-              <li><a href ="">Contact</a></li>
-            </ul>
-          </nav>
+      <div class = "container">
+          <a href = "" class = "logo"><img src = "images/logo.pn"></a> 
+            <nav>
+              <ul>
+
+                <li><a href ="">Home</a></li>
+                <li><a href ="">Work</a></li>
+                <li><a href ="">Services</a></li>
+                <li><a href ="">Clients</a></li>
+                <li><a href ="">Our Team</a></li>
+                <li><a href ="">Contact</a></li>
+
+              </ul>
+            </nav>
         </div>
-      </div>
     </header>
 
+    <section class ="slider">
+      <ul class = "slidr-carousel" id = "slider-carousel">
+        <li class = "image1">
+          <h2>GRANDINFINITY VARIETY <span>TRADING</span></h2>
+          <p>We got it all for you.</p>
+          <i class="fa fa-apple" aria-hidden="true"></i>
+          <i class="fa fa-android"></i>
+          <i class="fa fa-windows"></i>
+          <p><br>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, <br>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          </p><br>
+          <a href ="" class ="btn btn-half">Get Started</a>
+          <a href ="" class ="btn btn-full">Buy Now</a>
+        </li>
+         <li class = "image2">
+          <h2>GRANDINFITY VARIETY <span>TRADING</span></h2>
+          <p>We got it all for you.</p>
+          <i class="fa fa-apple" aria-hidden="true"></i>
+          <i class="fa fa-android"></i>
+          <i class="fa fa-windows"></i>
+          <p><br>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, <br>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          </p><br>
+          <a href ="" class ="btn btn-half">Get Started</a>
+          <a href ="" class ="btn btn-full">Buy Now</a>
+        </li>
+         <li class = "image3">
+          <h2>GRANDINFITY VARIETY <span>TRADING</span></h2>
+          <p>We got it all for you.</p>
+          <i class="fa fa-apple" aria-hidden="true"></i>
+          <i class="fa fa-android"></i>
+          <i class="fa fa-windows"></i>
+          <p><br>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, <br>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          </p><br>
+          <a href ="" class ="btn btn-half">Get Started</a>
+          <a href ="" class ="btn btn-full">Buy Now</a>
+        </li>
+      </ul>
+    </section>
+
+
+    <script src ="js/main.js"></script>
   </body>
 </htmL>
+
