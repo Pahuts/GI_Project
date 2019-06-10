683b6d2675e6a4458932cf1aba02d03d5800a8b1
Pahuts <luispahutan@gmail.com>
2019-06-10 10:32:44 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<htmL>
+    <head>
+      <title> GrandInfinity Variety Trading</title>
+      <link href="css/styles.css" rel="stylesheet" type="text/css">
+  
+       <!-- Latest compiled and minified CSS -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+      <!-- jQuery library -->
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+
+      <!-- Popper JS -->
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+
+      <!-- Latest compiled JavaScript -->
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    </head> 
+  <body>
+
+    <header>
+      <div class="container">
+        <div class="row">
+          <a href="" class="logo"><img src="images/logo.png"></a> 
+          <nav>
+            <ul>
+              <li><a href ="">Home</a></li>
+              <li><a href ="">Work</a></li>
+              <li><a href ="">Services</a></li>
+              <li><a href ="">Clients</a></li>
+              <li><a href ="">Contact</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+
+  </body>
+</htmL>
