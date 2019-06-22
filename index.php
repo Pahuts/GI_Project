f502fcd58f7d820f7c6767547ceff70cc27966f3
Pahuts <luispahutan@gmail.com>
2019-06-22 16:22:05 +0800

    added second section

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
     <head>
       <title> GrandInfinity Variety Trading</title>
       <link href="css/styles.css" rel="stylesheet" type="text/css">
-     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   
-       <!-- Latest compiled and minified CSS -->
+      <!-- Latest compiled and minified CSS -->
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 
       <!-- jQuery library -->
@@ -18,20 +18,18 @@
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     </head> 
   <body>
-
+    
     <header>
       <div class = "container">
-          <a href = "" class = "logo"><img src = "images/logo.pn"></a> 
+          <a href = "" class = "logo"><img src = "images/logo3.png"></a> 
             <nav>
               <ul>
-
                 <li><a href ="">Home</a></li>
                 <li><a href ="">Work</a></li>
                 <li><a href ="">Services</a></li>
                 <li><a href ="">Clients</a></li>
                 <li><a href ="">Our Team</a></li>
                 <li><a href ="">Contact</a></li>
-
               </ul>
             </nav>
         </div>
@@ -52,7 +50,7 @@
           <a href ="" class ="btn btn-full">Buy Now</a>
         </li>
          <li class = "image2">
-          <h2>GRANDINFITY VARIETY <span>TRADING</span></h2>
+          <h2>GRANDINFINITY VARIETY <span>TRADING</span></h2>
           <p>We got it all for you.</p>
           <i class="fa fa-apple" aria-hidden="true"></i>
           <i class="fa fa-android"></i>
@@ -64,7 +62,7 @@
           <a href ="" class ="btn btn-full">Buy Now</a>
         </li>
          <li class = "image3">
-          <h2>GRANDINFITY VARIETY <span>TRADING</span></h2>
+          <h2>GRANDINFINITY VARIETY <span>TRADING</span></h2>
           <p>We got it all for you.</p>
           <i class="fa fa-apple" aria-hidden="true"></i>
           <i class="fa fa-android"></i>
@@ -78,6 +76,19 @@
       </ul>
     </section>
 
+
+    <section class = "intro-area white" id = "intro">
+      <div class = "container">
+          <div class = "row">
+            <div class = "col-sm-12 text-center">
+              <h2>WHAT IS UP GRAND INFINITY</h2>
+              <div class = "sub-heading">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod <br>tempor incididunt ut labore et dolore magna aliqua. </p>
+              </div>
+            </div>
+          </div>
+      </div>
+    </section>
 
     <script src ="js/main.js"></script>
   </body>
